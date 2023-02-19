ac61cc4e4374466484334a84d068567fc9118876
Eric <goodtuesday@gmail.com>
2023-02-18 20:39:19 -0500

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,14 @@
+#Assignment 3
+
+#input prompt 
+
+base10Int = int(input("Pleas enter a base 10 integer for conversion to hexadecimal: "))
+
+# convert to base 16(hexadecimal)
+
+hexInt = hex(base10Int)
+
+#Print output
+
+print("The base 10 integer",base10Int ,"converted to hexadecimal equals:",hexInt)
+
